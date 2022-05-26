e4e0866463db1a0596fccac6a74a99ca53fbe322
AlexZheleznev <zhelezonsany@gmail.com>
2022-05-26 19:08:20 +0300

    Start project

---
+++ noticeSaver/src/main/java/com/project/noticeSaver/NoticeSaverApplication.java
@@ -0,0 +1,13 @@
+package com.project.noticeSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeSaverApplication.class, args);
+	}
+
+}
